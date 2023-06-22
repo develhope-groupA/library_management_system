9a7da0aa87ab2e954dd37fb3134438244e685b34
Tom MBOYA <tommboya044@gmail.com>
2023-06-22 18:08:58 +0300

    2

--- src/main/java/books/Book.java
+++ src/main/java/books/Book.java
@@ -1,48 +1,100 @@
 package books;
 
+import java.util.Set;
+import java.util.Map;
+import java.util.List;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class Book {
 
-    private String title;
-    private String author;
-    private int publicationYear;
+    // Instance variables
+    private Set<String> Titles; // Set to store book titles
+    private Map<String, String> Authors; // Map to store book authors mapped to book titles
+    private List<String[]> PublicationYears; // List to store book titles and publication years
+    private int totalBooks; // Total number of books in the library
 
-    public Book(String title, String kamar, int i) {
-        this.title = title;
-        this.author = author;
-        this.publicationYear = publicationYear;
+    // Constructor
+    public Book(String adventures, String makena, int i) {
+        Titles = new HashSet<>();
+        Authors = new HashMap<>();
+        PublicationYears = new ArrayList<>();
+        totalBooks = 0;
     }
 
-    public String getTitle() {
-        return title;
+    // Method to add a book to the library
+    public void addBook(String title, String author, int publicationYear) {
+        Titles.add(title);
+        Authors.put(author, title);
+        PublicationYears.add(new String[]{title, String.valueOf(publicationYear)});
+        totalBooks++;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    // Method to check if a book is in the library
+    public boolean isBookInLibrary(String title) {
+        return Titles.contains(title);
     }
 
-    public String getAuthor() {
-        return author;
+    // Method to get books by a specific author
+    public List<String> getBooksByAuthor(String author) {
+        List<String> books = new ArrayList<>();
+        for (String book : Authors.keySet()) {
+            if (Authors.get(book).equals(author)) {
+                books.add(book);
+            }
+        }
+        return books;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    // Method to get books published in a specific year
+    public List<String> getBooksPublishedInYear(int year) {
+        List<String> books = new ArrayList<>();
+        for (String[] book : PublicationYears) {
+            if (Integer.parseInt(book[1]) == year) {
+                books.add(book[0]);
+            }
+        }
+        return books;
     }
 
-    public int getPublicationYear() {
-        return publicationYear;
+    // Method to remove a book from the library
+    public void removeBook(String title) {
+        if (isBookInLibrary(title)) {
+            Titles.remove(title);
+            Authors.remove(title);
+            PublicationYears.removeIf(book -> book[0].equals(title));
+            totalBooks--;
+        } else {
+            System.out.println("The book is not in the library.");
+        }
     }
 
-    public void setPublicationYear(int publicationYear) {
-        this.publicationYear = publicationYear;
+    // Method to update the details of a book in the library
+    public void updateBookDetails(String title, String newTitle, String newAuthor, int newPublicationYear) {
+        if (isBookInLibrary(title)) {
+            Titles.remove(title);
+            Authors.remove(title);
+            PublicationYears.removeIf(book -> book[0].equals(title));
+
+            Titles.add(newTitle);
+            Authors.put(newAuthor, newTitle);
+            PublicationYears.add(new String[]{newTitle, String.valueOf(newPublicationYear)});
+        } else {
+            System.out.println("The book is not in the library.");
+        }
     }
 
-    @Override
-    public String toString() {
-        return String.format("Title: %s\nAuthor: %s\nPublication year: %d", title, author, publicationYear);
+
+    public Object getTitle() {
+        return Titles;
     }
 
+    public Object getAuthor() {
+        return Authors;
+    }
 
+    public Object getPublicationYears() {
+        return PublicationYears;
+    }
 }
-
-
-
